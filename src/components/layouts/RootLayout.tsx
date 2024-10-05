2c5fd6406bbedbc74bfd6b12e3f320bad666f861
Kalyan Chinna Bathula <41196585+kalyancharlie@users.noreply.github.com>
2024-10-06 00:38:23 +0530

    Add navbar to admin & vendor

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -3,10 +3,9 @@ import { Outlet } from "react-router-dom";
 
 const RootLayout: React.FC = () => {
   return (
-    <div>
-      Top Nav Bar outside Admin & Vendor
+    <>
       <Outlet />
-    </div>
+    </>
   );
 };
 
--- src/components/layouts/VendorLayout.tsx
+++ src/components/layouts/VendorLayout.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
+import { logoutVendor, selectVendorAuth } from "../../features/authSlice";
+import Navbar from "../Navbar";
+
 const VendorLayout: React.FC = () => {
+  const dispatch = useDispatch();
+  const vendorAuth = useSelector(selectVendorAuth);
+  const { name } = vendorAuth ?? {};
+
   return (
     <div>
-      <div>Vendor NavBar</div>
-      <div>
+      <Navbar
+        moduleName="Vendor Panel"
+        userName={name || "User"}
+        onLogout={() => {
+          dispatch(logoutVendor());
+        }}
+      />
+      <div style={{ padding: "1rem" }}>
         <Outlet />
       </div>
-      <div>Vendor Footer</div>
     </div>
   );
 };
