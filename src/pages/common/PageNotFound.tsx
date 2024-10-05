13c7e54d691a9ae954e1cadd6bf66c4de293bcef
Kalyan Chinna Bathula <41196585+kalyancharlie@users.noreply.github.com>
2024-10-06 00:33:19 +0530

    Update common pages

--- src/pages/common/PageNotFound.tsx
+++ src/pages/common/PageNotFound.tsx
@@ -1,7 +1,26 @@
+import { Button, Result } from "antd";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const PageNotFound: React.FC = () => {
-  return <div>PageNotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button
+          type="primary"
+          onClick={() => navigate("/login", { replace: true })}
+        >
+          Back Home
+        </Button>
+      }
+      style={{ paddingTop: "100px", textAlign: "center" }}
+    />
+  );
 };
 
 export default PageNotFound;
