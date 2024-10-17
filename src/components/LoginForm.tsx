9a2e301187f386a9cf716c5df4480135ffc9a92d
Kalyan Chinna Bathula <kalyancharlie@gmail.com>
2024-10-17 17:59:20 +0530

    Remove logs

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,61 +1,60 @@
-import React from "react";
-import { Form, Input, Button, message, Typography } from "antd";
-const { Text } = Typography;
+import React from 'react'
+import { Form, Input, Button, message, Typography } from 'antd'
+const { Text } = Typography
 
-import { IApiFormState } from "../interfaces/Common.interface";
-import { UserLoginResponse } from "../gql/query/user";
+import { IApiFormState } from '../interfaces/Common.interface'
+import { UserLoginResponse } from '../gql/query/user'
 
 export interface ILoginFormState {
-  role: "VENDOR" | "ADMIN";
-  mobileNumber: string;
-  otp: string;
+  role: 'VENDOR' | 'ADMIN'
+  mobileNumber: string
+  otp: string
 }
 
 export interface ILoginFormProps {
-  formState: ILoginFormState;
-  setFormState: React.Dispatch<React.SetStateAction<ILoginFormState>>;
+  formState: ILoginFormState
+  setFormState: React.Dispatch<React.SetStateAction<ILoginFormState>>
   onSubmit: (
     isNotValidForm?: string
-  ) => Promise<UserLoginResponse["user_login"] | undefined>;
-  apiState: IApiFormState;
+  ) => Promise<UserLoginResponse['user_login'] | undefined>
+  apiState: IApiFormState
 }
 const LoginForm: React.FC<ILoginFormProps> = ({
   formState,
   setFormState,
   onSubmit,
-  apiState,
+  apiState
 }) => {
-  const { loading, error } = apiState;
+  const { loading, error } = apiState
   // Handle form input changes
   const handleInputChange = (name: keyof typeof formState, value: string) => {
     setFormState((prevState) => ({
       ...prevState,
-      [name]: value,
-    }));
-  };
+      [name]: value
+    }))
+  }
 
   // Handle form submission
   const handleSubmit = () => {
     if (!formState.mobileNumber.match(/^\d{10}$/)) {
-      message.error("Please enter a valid 10-digit mobile number.");
-      onSubmit("Please enter a valid 10-digit mobile number.");
-      return;
+      message.error('Please enter a valid 10-digit mobile number.')
+      onSubmit('Please enter a valid 10-digit mobile number.')
+      return
     }
 
-    if (formState.otp === "") {
-      onSubmit("Please enter the OTP.");
-      message.error("Please enter the OTP.");
-      return;
+    if (formState.otp === '') {
+      onSubmit('Please enter the OTP.')
+      message.error('Please enter the OTP.')
+      return
     }
 
     // Api Call
-    console.log("Form Data Submitted:", formState);
-    onSubmit();
-  };
+    onSubmit()
+  }
 
   return (
-    <div style={{ maxWidth: "400px", margin: "50px auto", padding: "1rem" }}>
-      <h2 style={{ textAlign: "center", marginBottom: "30px" }}>Login</h2>
+    <div style={{ maxWidth: '400px', margin: '50px auto', padding: '1rem' }}>
+      <h2 style={{ textAlign: 'center', marginBottom: '30px' }}>Login</h2>
       <Form layout="vertical">
         {/* Role Selection */}
         {/* <Form.Item label="Select Role">
@@ -74,15 +73,15 @@ const LoginForm: React.FC<ILoginFormProps> = ({
           rules={[
             {
               required: true,
-              message: "Please enter your mobile number",
-            },
+              message: 'Please enter your mobile number'
+            }
           ]}
         >
           <Input
             maxLength={10}
             placeholder="Enter 10-digit mobile number"
             value={formState.mobileNumber}
-            onChange={(e) => handleInputChange("mobileNumber", e.target.value)}
+            onChange={(e) => handleInputChange('mobileNumber', e.target.value)}
           />
         </Form.Item>
 
@@ -92,14 +91,14 @@ const LoginForm: React.FC<ILoginFormProps> = ({
           rules={[
             {
               required: true,
-              message: "Please enter the OTP sent to your phone",
-            },
+              message: 'Please enter the OTP sent to your phone'
+            }
           ]}
         >
           <Input.Password
             placeholder="Enter OTP"
             value={formState.otp}
-            onChange={(e) => handleInputChange("otp", e.target.value)}
+            onChange={(e) => handleInputChange('otp', e.target.value)}
           />
         </Form.Item>
 
@@ -112,7 +111,7 @@ const LoginForm: React.FC<ILoginFormProps> = ({
         {error && <Text type="danger">{error}</Text>}
       </Form>
     </div>
-  );
-};
+  )
+}
 
-export default LoginForm;
+export default LoginForm
